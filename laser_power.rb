0c26096b6b4fe08bdb75aec328931f8a419ae069
Mathew Emerson <matdemerson@gmail.com>
2013-08-24 20:45:31 +1000

    Initial Commit...

---
+++ laser_power.rb
@@ -0,0 +1,10 @@
+# Works out the power required for the fricken laser beams
+# as per the Australian NBN standards.
+#
+# 0.35 db loss per kilometre
+# 0.10 db loss per fusion splice
+# 0.30 db loss per connection
+# 19 db loss at 32 way splitter
+#
+# Mathew Emerson - 24/08/13
+
