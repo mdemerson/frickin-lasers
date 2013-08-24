21e734c814b794f2026e3cd9827ddf9f3757e804
Mathew Emerson <matdemerson@gmail.com>
2013-08-25 00:25:07 +1000

    Working on premises details.
    Added premises calculations.
    
    Added method to ask questions for each premises.
    
    Added hash for storing premises.
    
    Added counter helper so that program goes up through house numbers,
    rather than down.
    
    Setup temporary printing to iterate over hash and make sure it's
    working.

--- laser_power.rb
+++ laser_power.rb
@@ -12,28 +12,51 @@
 #
 # Mathew Emerson - 24/08/13
 
+# Create array for premises
+premises = Hash.new
+premises_counter = 1
+
 # Methods
 
 def db_loss(distance, splices, connections)
   return (distance.to_f * 0.35) + (splices.to_f * 0.10) + (connections.to_f * 0.30)
 end
 
+def premises_query(number)
+  # Get premises details and put them into 'premises' hash.
+  print "What is the fibre distance (in KM's) from the ODF to premises number #{number}? "
+  premises_distance = gets.chomp()
+  print "How many splices between the ODF and premises number #{number}? "
+  premises_splices = gets.chomp()
+  print "how many connections between the ODF and premises number #{number}? "
+  premises_connections = gets.chomp()
+  #@premises["#{number.to_s}"] = db_loss(premises_distance, premises_splices, premises_connections)
+  return db_loss(premises_distance, premises_splices, premises_connections)
+end
 
-# Establish how many connections there are going to be
+
+# Establish how many connections there are going to be.
 print "How many premises are we running fibre to? "
-premises = gets.chomp()
+premises_amount = gets.to_i
 
-# get backbone details
+# Get backbone details.
 print "What is the distance from the OLT to the ODF (in KM's)? "
-backbone = gets.chomp()
+backbone_distance = gets.chomp()
 print "How many splices between OLT and ODF? "
 backbone_splices = gets.chomp()
 print "How many connections are between the OLT and ODF? "
 backbone_connections = gets.chomp()
 
-backbone_db_loss = db_loss(backbone, backbone_splices, backbone_connections) + 0.30 + 19
+backbone_db_loss = db_loss(backbone_distance, backbone_splices, backbone_connections) + 0.30 + 19
 
+while premises_counter <= premises_amount
+  premises["#{premises_counter.to_s}"] = premises_query(premises_counter)
+  premises_counter += 1
+end
 
+# Print out the results
+puts "OLT to ODF loss is #{backbone_db_loss}."
+premises.each { |number, value| puts "premises #{number} has loss of #{value}."}
 
 
 
