6f1c22fee13cf48c47c048aa7750720586d59e5f
Mathew Emerson <matdemerson@gmail.com>
2013-08-25 19:30:07 +1000

    Dual wavelength calculating.
    Now does both 1310nm & 1550nm at the same time instead of asking,
    this has caused the code to increase dramatically in size.
    
    Had to double up on most variables (for 1310 and 1550).
    
    Fixed some more formatting so each section of output is bunched.
    
    Moved premises questions out of method and into loop.

--- laser_power.rb
+++ laser_power.rb
@@ -14,44 +14,31 @@
 # Mathew Emerson - 24/08/13
 
 # Before we do anything else, find out the wavelength of the fibre.
-puts "what is the wavelength of your fibre?"
-print "'a' for 1550nm SMOF any other key for 1310: "
-wavelength = gets.chomp()
+# puts "what is the wavelength of your fibre?"
+# print "'a' for 1550nm SMOF any other key for 1310: "
+# wavelength = gets.chomp()
 
 # Create hashes.
-premises = Hash.new
-worst_case = Hash.new
-total_loss = Hash.new
+premises_1310 = Hash.new
+premises_1550 = Hash.new
+worst_case_1310 = Hash.new
+worst_case_1550 = Hash.new
+total_loss_1310 = Hash.new
+total_loss_1550 = Hash.new
 
 # Create counters and defaults.
 premises_counter = 1
-worst_case_counter = 0
-wavelength_name = "1550nm"
-optimal_light = 0
+worst_case_counter_1310 = 0
+worst_case_counter_1550 = 0
+optimal_light_1310 = 0
+optimal_light_1550 = 0
+
 
 ###### Methods ######
 
 # Perform calculations to get DB loss at different wavelengths
 def db_loss(distance, wavelength, splices, connections)
-  if wavelength == "a"
-    wavelength_calc = 0.21
-  else
-    wavelength_calc = 0.35
-  end
-  return (distance * wavelength_calc) + (splices * 0.10) + (connections * 0.30)
-end
-
-# Ask questions for each premises.
-def premises_query(number, wavelength)
-  # Get premises details and put them into 'premises' hash.
-  puts ""
-  print "What is the fibre distance (in KM's) from the ODF to premises #{number}? "
-  premises_distance = gets.to_f
-  print "How many splices between the ODF and premises #{number}? "
-  premises_splices = gets.to_i
-  print "how many connections between the ODF and premises #{number} (including ODF)? "
-  premises_connections = gets.to_i
-  return db_loss(premises_distance, wavelength, premises_splices, premises_connections)
+  return (distance * wavelength) + (splices * 0.10) + (connections * 0.30)
 end
 
 # Convert dBm to mW
@@ -67,11 +54,6 @@ end
 
 ###### Program ######
 
-# Change the wavelength name if necessary.
-unless wavelength == "a"
-  wavelength_name = "1310nm"
-end
-
 # Establish how many connections there are going to be.
 puts ""
 print "How many premises are we running fibre to? "
@@ -86,25 +68,43 @@ backbone_splices = gets.to_i
 print "How many connections are between the OLT and ODF? "
 backbone_connections = gets.to_i
 
-backbone_db_loss = db_loss(backbone_distance, wavelength, backbone_splices, backbone_connections) + 19
+backbone_db_loss_1310 = db_loss(backbone_distance, 0.35, backbone_splices, backbone_connections) + 19
+backbone_db_loss_1550 = db_loss(backbone_distance, 0.21, backbone_splices, backbone_connections) + 19
 
-# Calls the premises query
+# Calls the premises querys
 while premises_counter <= premises_amount
-  premises["#{premises_counter.to_s}"] = premises_query(premises_counter, wavelength)
+  puts ""
+  print "What is the fibre distance (in KM's) from the ODF to premises #{premises_counter}? "
+  premises_distance = gets.to_f
+  print "How many splices between the ODF and premises #{premises_counter}? "
+  premises_splices = gets.to_i
+  print "how many connections between the ODF and premises #{premises_counter} (including ODF)? "
+  premises_connections = gets.to_i
+
+  premises_1310["#{premises_counter.to_s}"] = db_loss(premises_distance, 0.35, premises_splices, premises_connections)
+  premises_1550["#{premises_counter.to_s}"] = db_loss(premises_distance, 0.21, premises_splices, premises_connections)
+
   premises_counter += 1
 end
 
 # Work out which premises has the highest loss
-premises.each do |number, value|
-  unless value < worst_case_counter
-    worst_case_counter = value
-    worst_case = {"#{number.to_s}" => value}
+premises_1310.each do |number, value|
+  unless value < worst_case_counter_1310
+    worst_case_counter_1310 = value
+    worst_case_1310 = {"#{number.to_s}" => value}
+  end
+end
+
+premises_1550.each do |number, value|
+  unless value < worst_case_counter_1550
+    worst_case_counter_1550 = value
+    worst_case_1550 = {"#{number.to_s}" => value}
   end
 end
 
 # Work out the optimal light
-optimal_light = -18 + (worst_case.values.first + backbone_db_loss)
-
+optimal_light_1310 = -18 + (worst_case_1310.values.first.to_f + backbone_db_loss_1310)
+optimal_light_1550 = -18 + (worst_case_1550.values.first.to_f + backbone_db_loss_1550)
 
 ###### Print out the results ######
 
@@ -112,28 +112,38 @@ optimal_light = -18 + (worst_case.values.first + backbone_db_loss)
 40.times do puts "" end
 
 # A bit of basic info.
-puts "We are using #{wavelength_name} SMOF to go to #{premises_amount} premises."
+puts "OLT to ODF loss is #{backbone_db_loss_1310.round(2)} @ 1310nm."
+puts "OLT to ODF loss is #{backbone_db_loss_1550.round(2)} @ 1550nm."
 puts ""
-puts "OLT to ODF loss is #{backbone_db_loss.round(2)}."
-puts ""
-puts "Premises #{worst_case.keys.first} has the greatest loss, -#{(worst_case.values.first).round(2)} dB (-#{(worst_case.values.first + backbone_db_loss).round(2)} dB total) ."
+puts "Premises #{worst_case_1310.keys.first} has the greatest loss, -#{(worst_case_1310.values.first).round(2)} dB (-#{(worst_case_1310.values.first + backbone_db_loss_1310).round(2)} dB total) @ 1310nm."
+puts "Premises #{worst_case_1550.keys.first} has the greatest loss, -#{(worst_case_1550.values.first).to_f.round(2)} dB (-#{(worst_case_1550.values.first.to_f + backbone_db_loss_1550).to_f.round(2)} dB total) @ 1550nm."
 puts ""
 
 # Print out loss from ODF to each premises.
-premises.each do |number, value|
-  puts "premises #{number} has loss of -#{value.round(2)} dB, -#{(value + backbone_db_loss).round(2)} dB total."
-  total_loss["#{number.to_s}"] = (value + backbone_db_loss).round(2)
+premises_1310.each do |number, value|
+  puts "premises #{number} has loss of -#{value.round(2)} dB, -#{(value + backbone_db_loss_1310).round(2)} dB total @ 1310nm."
+  total_loss_1310["#{number.to_s}"] = (value + backbone_db_loss_1310).round(2)
 end
+puts ""
+
+# Print out loss from ODF to each premises.
+premises_1550.each do |number, value|
+  puts "premises #{number} has loss of -#{value.round(2)} dB, -#{(value + backbone_db_loss_1550).round(2)} dB total @ 1550nm."
+  total_loss_1550["#{number.to_s}"] = (value + backbone_db_loss_1550).round(2)
+end
+puts ""
 
 # Print out light reading at each premises.
-total_loss.each do |number, value|
-  puts ""
-  puts "premises #{number} will have a light reading of #{(optimal_light - value).round(2)} dBm."
-  if (optimal_light - value) > -8
-    puts "*** THIS CONNECTION WILL NEED TO BE ATTENUATED ***"
-  end
+total_loss_1310.each do |number, value|
+  puts "premises #{number} will have a light reading of #{(optimal_light_1310 - value).round(2)} dBm @ 1310nm."
 end
+puts ""
+
+total_loss_1550.each do |number, value|
+  puts "premises #{number} will have a light reading of #{(optimal_light_1550 - value).round(2)} dBm @ 1550nm."
+end
+puts ""
 
 # Print out required laser power.
-puts ""
-puts "You will require a laser capable of emitting at least #{dbm_to_mw(optimal_light).round(2)} mW."
+puts "You will require a laser capable of emitting at least #{dbm_to_mw(optimal_light_1310).round(2)} mW for 1310nm."
+puts "You will require a laser capable of emitting at least #{dbm_to_mw(optimal_light_1550).round(2)} mW for 1550nm."
