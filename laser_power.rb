4ab4e699fd1fb3132b9b495ea6038614c7f1e83a
Mathew Emerson <matdemerson@gmail.com>
2013-08-25 14:09:08 +1000

    Added Wavelengths
    Now immediately asks for wavelength.
    Now does calculations based on wavelength.
    Neatened up the printed output.
    Has friendly name for wavelength.

--- laser_power.rb
+++ laser_power.rb
@@ -13,30 +13,51 @@
 #
 # Mathew Emerson - 24/08/13
 
+# Before we do anything else, find out the wavelength of the fibre.
+puts "what is the wavelength of your fibre?"
+print "'a' for 1550nm SMOF any other key for 1310: "
+wavelength = gets.chomp()
+
 # Create hashes.
 premises = Hash.new
 worst_case = Hash.new
 
-# Create counters.
+# Create counters and defaults.
 premises_counter = 1
 worst_case_counter = 0
+wavelength_name = "1550nm"
 
-# Methods
-def db_loss(distance, splices, connections)
-  # Perform calculations for 1310nm SMOF
-  return (distance * 0.35) + (splices * 0.10) + (connections * 0.30)
+
+###### Methods ######
+
+# Perform calculations to get DB loss at different wavelengths
+def db_loss(distance, wavelength, splices, connections)
+  if wavelength == "a"
+    wavelength_calc = 0.21
+  else
+    wavelength_calc = 0.35
+  end
+  return (distance * wavelength_calc) + (splices * 0.10) + (connections * 0.30)
 end
 
 # Ask questions for each premises.
-def premises_query(number)
+def premises_query(number, wavelength)
   # Get premises details and put them into 'premises' hash.
-  print "What is the fibre distance (in KM's) from the ODF to premises number #{number}? "
+  print "What is the fibre distance (in KM's) from the ODF to premises #{number}? "
   premises_distance = gets.to_f
-  print "How many splices between the ODF and premises number #{number}? "
+  print "How many splices between the ODF and premises #{number}? "
   premises_splices = gets.to_i
-  print "how many connections between the ODF and premises number #{number}? "
+  print "how many connections between the ODF and premises #{number} (including ODF)? "
   premises_connections = gets.to_i
-  return db_loss(premises_distance, premises_splices, premises_connections)
+  return db_loss(premises_distance, wavelength, premises_splices, premises_connections)
+end
+
+
+###### Program ######
+
+# Change the wavelength name if necessary.
+unless wavelength == "a"
+  wavelength_name = "1310nm"
 end
 
 # Establish how many connections there are going to be.
@@ -51,11 +72,11 @@ backbone_splices = gets.to_i
 print "How many connections are between the OLT and ODF? "
 backbone_connections = gets.to_i
 
-backbone_db_loss = db_loss(backbone_distance, backbone_splices, backbone_connections) + 0.30 + 19
+backbone_db_loss = db_loss(backbone_distance, wavelength, backbone_splices, backbone_connections) + 19
 
 # Calls the premises query
 while premises_counter <= premises_amount
-  premises["#{premises_counter.to_s}"] = premises_query(premises_counter)
+  premises["#{premises_counter.to_s}"] = premises_query(premises_counter, wavelength)
   premises_counter += 1
 end
 
@@ -67,8 +88,14 @@ premises.each do |number, value|
   end
 end
 
-# Print out the results
+
+###### Print out the results ######
+puts "" * 5
+puts "We are using #{wavelength_name} SMOF to go to #{premises_amount} premises."
+puts ""
 puts "OLT to ODF loss is #{backbone_db_loss}."
+puts ""
 premises.each {|number, value| puts "premises #{number} has loss of #{value} dB."}
+puts ""
 puts "premises #{worst_case.keys.first} has the greatest loss (#{worst_case.values.first} dB) and a total loss of #{worst_case.values.first + backbone_db_loss} dB."
 
